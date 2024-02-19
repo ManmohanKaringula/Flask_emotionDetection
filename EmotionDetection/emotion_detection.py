f1daf92ee50ffec11fdcc45c94c80a7244b80026
manmohan <manmohanreddyk.999@gmail.com>
2024-02-19 05:08:41 -0500

    This is reagrding the error handling of the server based on https status code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,7 +7,13 @@ def emotion_detector(text_to_analyse):
     Input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = Input_json, headers=Headers)
     formatted_response = json.loads(response.text)
-    formatted_response = formatted_response["emotionPredictions"][0]["emotion"] 
-    dominant_emotion = max(formatted_response, key=formatted_response.get)
-    formatted_response["dominant_emotion"] = dominant_emotion
-    return formatted_response
+    if response.status_code == 200:
+        formatted_response = formatted_response["emotionPredictions"][0]["emotion"] 
+        dominant_emotion = max(formatted_response, key=formatted_response.get)
+        formatted_response["dominant_emotion"] = dominant_emotion
+        return formatted_response
+    elif response.status_code == 400:
+        labels=["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]
+        formatted_response = { key: None for key in labels }
+        return formatted_response
+    
--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -10,12 +7,14 @@ app= Flask("Emotion Detection")
 def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    formatted_output = "For the given statement the system response is "
-    formatted_output += ", ".join([f"{key}: {value}" for key, value in response.items() if key != "sadness" and key != "dominant_emotion"])
-    formatted_output += " and {}: {}".format("sadness", response.get("sadness", ""))
-    formatted_output += "{} {}".format(". The dominant emotion is", response.get("dominant_emotion", ""))
-    return formatted_output
-
+    if response["dominant_emotion"] != None: 
+        formatted_output = "For the given statement the system response is "
+        formatted_output += ", ".join([f"{key}: {value}" for key, value in response.items() if key != "sadness" and key != "dominant_emotion"])
+        formatted_output += " and {}: {}".format("sadness", response.get("sadness", ""))
+        formatted_output += "{} {}".format(". The dominant emotion is", response.get("dominant_emotion", ""))
+        return formatted_output
+    else:
+        return "Invalid text! Please try again!"
 
 
 
